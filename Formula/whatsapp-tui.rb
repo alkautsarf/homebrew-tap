27d0e23f0095967651c836a35a0224e8ffaeb2d4
Alkautsar F <alkautsarsol22@gmail.com>
2026-04-11 10:32:26 +0700

    whatsapp-tui 0.5.2

--- Formula/whatsapp-tui.rb
+++ Formula/whatsapp-tui.rb
@@ -1,9 +1,9 @@
 class WhatsappTui < Formula
   desc "WhatsApp TUI client with vim keybindings"
   homepage "https://github.com/alkautsarf/whatsapp-tui-ts"
-  url "https://github.com/alkautsarf/whatsapp-tui-ts/releases/download/v0.5.1/whatsapp-tui-v0.5.1-source.tar.gz"
-  sha256 "504b29554f6f215642ecdd8994c50efcc6f58379c1c6c998c1221e8bc2f81156"
-  version "0.5.1"
+  url "https://github.com/alkautsarf/whatsapp-tui-ts/releases/download/v0.5.2/whatsapp-tui-v0.5.2-source.tar.gz"
+  sha256 "447ef178bca13f5fff9e855c67ef78e4ae019b75e533ea1802a98bd68eb58647"
+  version "0.5.2"
   depends_on "oven-sh/bun/bun"
 
   def install
